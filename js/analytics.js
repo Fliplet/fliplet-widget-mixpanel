0a3755bb027a7d4a2353778c1a95e075d0847bb9
Ricardo Seromenho <ricardo@seromenho.com>
2016-11-24 18:03:26 +0000

    Use new Fliplet.Analytics.subscribe

--- js/analytics.js
+++ js/analytics.js
@@ -8,8 +8,18 @@
   var trackerData = Fliplet.Widget.getData(tag.data('mixpanel-id'));
   var trackerToken = trackerData[Fliplet.Env.get('platform') === 'web' ? 'webTracker' : 'nativeTracker'];
 
+  function trackEvent(data) {
+    if (!data.category) {
+      return;
+    }
+
+    var category = data.category;
+    delete data.category;
+    mixpanel.track(category, data);
+  }
+
   Fliplet.Navigator.onReady().then(function () {
-      mixpanel.init(trackerToken, { persistence: 'localStorage'});
-      mixpanel.track("Track test");
+      mixpanel.init(trackerToken);
+      Fliplet.Analytics2.subscribe('trackEvent', trackEvent)
     });
 })();
