bccdd5371f31191b9bb4b7e8819c371f962fd436
Tony Wu <twu@fliplet.com>
2019-03-30 21:24:10 +0000

    Skip initialization if mixpanel tracking ID is not provided

--- js/analytics.js
+++ js/analytics.js
@@ -17,6 +17,11 @@
   var eventsStorageKey = 'mixpanel-events-' + widgetUuid;
   var propertiesStorageKey = 'mixpanel-properties-' + widgetUuid;  // Used to store super properties
 
+  if (!trackerToken) {
+    // No tracker token provided
+    return;
+  }
+
   Fliplet.Navigator.onOnline(function trackStored() {
     Fliplet.Storage.get(eventsStorageKey)
       .then(function (trackings) {
@@ -85,13 +90,13 @@
       // Subscribe to other hooks than Analytics
       Fliplet.Hooks.on('onUserVerified', function (data) {
         Fliplet.Analytics.isTrackingEnabled().then(function(userEnabledTracking) {
-          // TODO: Remove next line once we implement this on core. Providers should 
+          // TODO: Remove next line once we implement this on core. Providers should
           // safely subcribe to hooks without checking for userEnabledTracking
           // This is also here and not before the hook atm because concurrency issues
           if (!userEnabledTracking) {
             return;
           }
-          
+
           if (!data || !data.id) {
             return;
           }
